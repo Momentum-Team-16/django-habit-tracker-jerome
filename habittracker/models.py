c98342db4294d04d12ad79664b5f6c2ce4569af2
jeromecox <jeromecox@hotmail.com>
2023-01-17 16:04:35 -0600

    added habit and record models

--- habittracker/models.py
+++ habittracker/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 
@@ -11,3 +12,23 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Habit(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    target = models.IntegerField(validators=[MinValueValidator(0)], blank=True, null=True)
+    unit_measure = models.TextField(max_length=100, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='habits')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} {self.target} {self.unit_measure}'
+
+
+class Record(models.Model):
+    date = models.DateField()
+    quantity = models.IntegerField(validators=[MinValueValidator(0)], blank=True, null=True)
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE, related_name='records')
+
+    def __str__(self):
+        return f'{self.habit} on {self.date}'
