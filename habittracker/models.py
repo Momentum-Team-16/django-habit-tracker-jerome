5979d81bdec6e5f465500dcbff5a745a2877202f
jeromecox <jeromecox@hotmail.com>
2023-01-17 16:32:49 -0600

    added blank static files and templates

--- habittracker/models.py
+++ habittracker/models.py
@@ -9,6 +9,7 @@ class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True, null=True)
     location = models.CharField(max_length=30, blank=True, null=True)
     birth_date = models.DateField(null=True, blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
 
     def __str__(self):
         return self.username
--- habittracker/views.py
+++ habittracker/views.py
@@ -1,3 +1,6 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from .models import User, Habit, Record
+
 
 # Create your views here.
