6dc930707a42ee9b71e57f7ceba879bdc4ee6d0a
jeromecox <jeromecox@hotmail.com>
2023-01-18 12:26:17 -0600

    added STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
