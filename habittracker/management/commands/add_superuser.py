e82c31c823b39cb8e46680e32b4753c7598206ab
jeromecox <jeromecox@hotmail.com>
2023-01-18 15:42:48 -0600

    testing deployment1

--- habittracker/management/commands/add_superuser.py
+++ habittracker/management/commands/add_superuser.py
@@ -10,17 +10,16 @@ class Command(BaseCommand):
     help = "Create a superuser in production"
 
     def handle(self, *args, **options):
-        if settings.RENDER:
-            user, created = User.objects.get_or_create(
-                username=settings.DJANGO_SUPERUSER_USERNAME
-            )
-            if created:
-                user.email = settings.DJANGO_SUPERUSER_EMAIL
-                user.set_password(settings.DJANGO_SUPERUSER_PASSWORD)
-                user.is_superuser = True
-                user.is_staff = True
-                user.save()
-                msg = self.style.SUCCESS(f"Superuser {settings.DJANGO_SUPERUSER_USERNAME} added to database.")
-            else:
-                msg = self.style.WARNING(f"Superuser {settings.DJANGO_SUPERUSER_USERNAME} already exists.")
-            self.stdout.write(msg)
+        user, created = User.objects.get_or_create(
+            username=settings.DJANGO_SUPERUSER_USERNAME
+        )
+        if created:
+            user.email = settings.DJANGO_SUPERUSER_EMAIL
+            user.set_password(settings.DJANGO_SUPERUSER_PASSWORD)
+            user.is_superuser = True
+            user.is_staff = True
+            user.save()
+            msg = self.style.SUCCESS(f"Superuser {settings.DJANGO_SUPERUSER_USERNAME} added to database.")
+        else:
+            msg = self.style.WARNING(f"Superuser {settings.DJANGO_SUPERUSER_USERNAME} already exists.")
+        self.stdout.write(msg)
