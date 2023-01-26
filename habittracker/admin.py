b0a1678cff597c2171450cf22a7947b3cc9d492c
jeromecox <jeromecox@hotmail.com>
2023-01-26 10:06:07 -0600

    new user creation works

--- habittracker/admin.py
+++ habittracker/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Habit, Record
+from .forms import CustomUserCreationForm
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Habit)
 admin.site.register(Record)
--- habittracker/forms.py
+++ habittracker/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import Habit, Record
+from .models import Habit, Record, User
+from django.contrib.auth.forms import UserCreationForm, ValidationError
+from django.contrib.auth import get_user_model
 
 
 class HabitForm(forms.ModelForm):
@@ -12,3 +14,18 @@ class RecordForm(forms.ModelForm):
     class Meta:
         model = Record
         fields = ('date', 'quantity')
+
+
+# https://code.djangoproject.com/ticket/19353
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+
+    def clean_username(self):
+        """Reject usernames that differ only in case."""
+        username = self.cleaned_data.get("username")
+        UserModel = get_user_model()
+        if username and UserModel.objects.filter(username__iexact=username).exists():
+            raise ValidationError(self.error_messages["unique"], code="unique")
+        else:
+            return username
